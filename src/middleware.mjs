5b9a5a689eb1c88166f47768ae6a7f83b711697c
Mike Marcacci <mike.marcacci@gmail.com>
2018-07-05 11:25:32 -0600

    Revert "use eos to detect stream finish"
    This reverts commit 823b979ff02d3228576c52bb13cf3687468186a5.

--- src/middleware.mjs
+++ src/middleware.mjs
@@ -1,7 +1,6 @@
 import Busboy from 'busboy'
 import objectPath from 'object-path'
 import Capacitor from 'fs-capacitor'
-import eos from 'end-of-stream'
 import {
   SPEC_URL,
   MaxFileSizeUploadError,
@@ -196,6 +195,8 @@ export const processRequest = (
             upload.reject(
               new FileMissingUploadError('File missing in the request.')
             )
+
+      finish()
     })
 
     parser.on('error', err => {
@@ -207,6 +208,8 @@ export const processRequest = (
 
       if (currentStream) currentStream.destroy(err)
     })
+
+    request.on('end', finish)
 
     request.on('close', () => {
       if (map)
@@ -225,8 +228,6 @@ export const processRequest = (
           )
         )
     })
-
-    eos(parser, finish)
 
     request.pipe(parser)
   })
