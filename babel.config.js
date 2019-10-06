1f51499012008b91455abfb60d4251064c5f8ab4
Jayden Seric <me@jaydenseric.com>
2019-10-06 14:24:11 +1100

    Ignore the Babel _interopRequireDefault helper for code coverage.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,25 @@
+/**
+ * A Babel plugin that adds `istanbul ignore next` comments before Babel
+ * `_interopRequireDefault` function declarations, to allow 100% code coverage.
+ * @kind function
+ * @name babelPluginIstanbulIgnoreBabelInterop
+ * @returns {Function} The Babel plugin.
+ * @ignore
+ */
+const babelPluginIstanbulIgnoreBabelInterop = () => ({
+  visitor: {
+    FunctionDeclaration(path) {
+      if (path.node.id.name === '_interopRequireDefault')
+        path.addComment('leading', ' istanbul ignore next', true)
+    }
+  }
+})
+
 module.exports = {
-  comments: false,
+  shouldPrintComment: comment =>
+    // Preserve Istanbul ignore comments.
+    /@license|@preserve|istanbul ignore/.test(comment),
+  plugins: [babelPluginIstanbulIgnoreBabelInterop],
   presets: [
     [
       '@babel/env',
