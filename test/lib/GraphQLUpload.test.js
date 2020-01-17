73ef939ef16a40358eb80e0a54c5c3ead725b2fe
Jayden Seric <me@jaydenseric.com>
2020-01-17 19:02:03 +1100

    Ensure GraphQLUpload scalar errors when parsing invalid values.
    Fixes https://github.com/jaydenseric/graphql-upload/issues/175 .

--- test/lib/GraphQLUpload.test.js
+++ test/lib/GraphQLUpload.test.js
@@ -1,10 +1,32 @@
 'use strict'
 
-const { throws } = require('assert')
+const { doesNotThrow, throws } = require('assert')
 const { parseValue } = require('graphql')
 const GraphQLUpload = require('../../lib/GraphQLUpload')
+const Upload = require('../../lib/Upload')
 
 module.exports = tests => {
+  tests.add('`GraphQLUpload` scalar `parseValue` with a valid value.', () => {
+    doesNotThrow(() => {
+      GraphQLUpload.parseValue(new Upload())
+    })
+  })
+
+  tests.add(
+    '`GraphQLUpload` scalar `parseValue` with an invalid value.',
+    () => {
+      throws(
+        () => {
+          GraphQLUpload.parseValue(true)
+        },
+        {
+          name: 'GraphQLError',
+          message: 'Upload value invalid.'
+        }
+      )
+    }
+  )
+
   tests.add('`GraphQLUpload` scalar `parseLiteral`.', () => {
     throws(
       () => {
