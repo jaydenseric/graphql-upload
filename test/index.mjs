1dcf3893d21de6da3048891687c411afecc5858e
Jayden Seric <me@jaydenseric.com>
2021-11-30 16:20:54 +1100

    Rename imports in the test index module.

--- test/index.mjs
+++ test/index.mjs
@@ -1,18 +1,18 @@
 import TestDirector from "test-director";
-import testIgnoreStream from "./private/ignoreStream.test.mjs";
-import testGraphQLUpload from "./public/GraphQLUpload.test.mjs";
-import testUpload from "./public/Upload.test.mjs";
-import testGraphqlUploadExpress from "./public/graphqlUploadExpress.test.mjs";
-import testGraphqlUploadKoa from "./public/graphqlUploadKoa.test.mjs";
-import testProcessRequest from "./public/processRequest.test.mjs";
+import test_ignoreStream from "./private/ignoreStream.test.mjs";
+import test_GraphQLUpload from "./public/GraphQLUpload.test.mjs";
+import test_Upload from "./public/Upload.test.mjs";
+import test_graphqlUploadExpress from "./public/graphqlUploadExpress.test.mjs";
+import test_graphqlUploadKoa from "./public/graphqlUploadKoa.test.mjs";
+import test_processRequest from "./public/processRequest.test.mjs";
 
 const tests = new TestDirector();
 
-testIgnoreStream(tests);
-testGraphQLUpload(tests);
-testGraphqlUploadExpress(tests);
-testGraphqlUploadKoa(tests);
-testProcessRequest(tests);
-testUpload(tests);
+test_ignoreStream(tests);
+test_GraphQLUpload(tests);
+test_graphqlUploadExpress(tests);
+test_graphqlUploadKoa(tests);
+test_processRequest(tests);
+test_Upload(tests);
 
 tests.run();
