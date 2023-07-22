2fbe8951434c35eee0103955ce1b92e6fcceaad1
Banks Nussman <banks@nussman.us>
2023-07-22 16:45:57 -0400

    fuck it, just publish a working package

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,4 +5,6 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   dts: true,
   minify: true,
+  // We have to bundle fs-capacitor because the author is too lazy to support cjs
+  noExternal: ['fs-capacitor']
 });
